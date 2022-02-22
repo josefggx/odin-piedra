10fda1ff1f2ad9a068d44afab8571711b885f231
josefgg <josefgg58@gmail.com>
2022-02-22 14:39:18 -0500

    Added UI to the JS logic

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 let playerCount = 0;
 let computerCount = 0;
 let i = 0;
+const playAgainDiv = document.getElementById("playAgainDiv");
+const casesArray = [
+    "Draw",
+    "You Lose! Paper beats Rock",
+    "You Won! Rock beats Scissors",
+    "You Won! Paper beats Rock",
+    "You Lose! Scissors beats Paper",
+    "You Lose! Rock beats Scissors",
+    "You Won! Scissors beats Paper",
+]
+
+const rock = document.getElementById("rock");
+rock.addEventListener("click", () => { oneRound("rock") })
+
+const paper = document.getElementById("paper");
+paper.addEventListener("click", () => { oneRound("paper") })
+
+const scissors = document.getElementById("scissors");
+scissors.addEventListener("click", () => { oneRound("scissors") })
 
 const computerPlay = () => {
     const playList = ["Rock", "Paper", "Scissors"];
@@ -9,56 +28,121 @@ const computerPlay = () => {
 }
 
 const gameReset = () => {
-    playerCount = 1;
-    computerCount = 1;
+    playerCount = 0;
+    computerCount = 0;
     i = 0;
 }
 
-const oneRound = (playerSelection, computerSelection) => {
+const createElement = (text) => {
+    const newText = document.getElementById("textDiv");
+    newText.classList.add("text-log");
+    newText.textContent = text;
+}
+
+const showComputerSelection = (text) => {
+    const show = document.getElementById("computerSelection");
+    show.textContent = text;
+}
+
+const showScore = (text) => {
+    const score = document.getElementById("score");
+    score.textContent = text;
+}
+
+const announceWinner = (text) => {
+    const winner = document.getElementById("winner");
+    winner.textContent = text;
+}
+
+const gameClear = () => {
+    createElement("");
+    showComputerSelection("");
+    showScore("Player 0 - 0 Computer");
+    announceWinner("");
+}
+
+const blockButtons = () => {
+    // @ts-ignore
+    document.querySelectorAll(".playerButton").forEach(e => e.disabled = true);
+}
+
+const playAgainButton = () => {
+    const button = document.createElement('button');
+    button.classList.add('playAgainButton');
+    button.textContent = 'Play Again';
+    playAgainDiv.appendChild(button);
+}
+
+const unlockButtons = () => {
+    const btn = document.querySelector(".playAgainButton");
+    btn.addEventListener('click', () => {
+        // @ts-ignore
+        document.querySelectorAll(".playerButton").forEach(e => e.disabled = false);
+        gameClear();
+        const playAgainBtn = document.querySelector(".playAgainButton");
+        playAgainBtn.remove();
+    });
+}
+
+const count = () => {
+    i = i + 1;
+    console.log(`Ronda #${i}`);
+    showScore(`Player ${playerCount} - ${computerCount} Computer`);
+    if(playerCount === 5) {
+        announceWinner(`You Won!!!`);
+        gameReset();
+        blockButtons();
+        playAgainButton();
+        unlockButtons();
+        return;
+    } else if(computerCount === 5) {
+        announceWinner(`Computer Won!!!`);
+        gameReset();
+        blockButtons();
+        playAgainButton();
+        unlockButtons();
+        return;
+    }
+    return;
+}
+
+const oneRound = (playerSelection) => {
     const player = playerSelection.toLowerCase();
-    const computer = computerSelection.toLowerCase();
+    const computer = computerPlay().toLowerCase();
+    showComputerSelection(`computer selection was: ${computer}`);
     if(player === computer) {
-        return "Draw!"
+        createElement(casesArray[0]);
+        count();
+        return console.log(casesArray[0]);
     } else if(player === "rock" && computer === "paper") {
         computerCount++;
-        return "You Lose! Paper beats Rock";
+        createElement(casesArray[1]);
+        count();
+        return console.log(casesArray[1]);
     } else if(player === "rock" && computer === "scissors") {
         playerCount++;
-        return "You Won! Rock beats Scissors";
+        createElement(casesArray[2]);
+        count();
+        return console.log(casesArray[2]);
     } else if(player === "paper" && computer === "rock") {
         playerCount++;
-        return "You Won! Paper beats Rock";
+        createElement(casesArray[3]);
+        count();
+        return console.log(casesArray[3]);
     } else if(player === "paper" && computer === "scissors") {
         computerCount++;
-        return "You Lose! Scissors beats Paper";
+        createElement(casesArray[4]);
+        count();
+        return console.log(casesArray[4]);
     } else if(player === "scissors" && computer === "rock") {
         computerCount++;
-        return "You Lose! Rock beats Scissors";
+        createElement(casesArray[5]);
+        count();
+        return console.log(casesArray[5]);
     } else if(player === "scissors" && computer === "paper") {
         playerCount++;
-        return "You Won! Scissors beats Paper";
+        createElement(casesArray[6]);
+        count();
+        return console.log(casesArray[6]);
     }
 }
-
-const game = () => {
-    while(playerCount !== 5 && computerCount !== 5) {
-        i = i + 1;
-        const playerSelection = prompt("Rock, Paper or Scissors?");
-        const computerSelection = computerPlay();
-        console.log(`Ronda #${i}`)
-        console.log(oneRound(playerSelection, computerSelection));
-        console.log(`Computer selection was: ${computerSelection}`)
-        console.log(`Player ${playerCount} - ${computerCount} Computer`)
-    }
-    if(playerCount === 5) {
-        console.log(`You Won: Player ${playerCount} - ${computerCount} Computer`);
-        gameReset();
-        return "Thanks for playing!"
-    } else if(computerCount === 5) {
-        console.log(`You Lose: Player ${playerCount} - ${computerCount} Computer`);
-        gameReset();
-        return "Thanks for playing!"
-    }
-}
-
-console.log(game());
